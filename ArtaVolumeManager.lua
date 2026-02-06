8dd96c28013fc40c0d8785b081a9dbeaa6940839
Artanis <fabrizio+git@fabriziotobia.it>
2026-02-06 14:42:13 +0100

    Update ArtaVolumeManager.lua
    Added the initial skeleton.

--- ArtaVolumeManager.lua
+++ ArtaVolumeManager.lua
@@ -11,3 +11,18 @@ local TimeSinceLastUpdate = 0
 function ShowMessage(msg)
 	print ("|c398fdf99ArtaVolumeManager: |cffffffff" ..msg)
 end
+
+-- Function called when the OnLoad event is fired
+function AVM_OnLoad(self, event,...) 
+    self:RegisterEvent("ADDON_LOADED")	
+end
+
+
+-- Function called when the an event is fired
+function AVM_OnEvent(self, event, ...)	
+	if event == "ADDON_LOADED" and ... == "ArtaVolumeManager" then
+		ShowMessage("Loaded")
+		-- Since we are already responding to the event, we don't need to keep listening to it and we unregister it
+		self:UnregisterEvent("AddOn_LOADED")
+	end	
+end
